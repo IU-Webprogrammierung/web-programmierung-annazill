64cde984770090893e4cb7cd6eeb0c59f48c2bb2
Anna Zillmann <anna-tabea.zillmann@iubh-fernstudium.de>
2025-03-14 22:59:21 +0100

    feat: add responsive navigation bar

--- javaScript/loader.js
+++ javaScript/loader.js
@@ -1,1 +1,24 @@
 $( "footer" ).load( "components/footerContent.html" );
+
+function toggelBurgerMenuInNavbar() {
+    var navbarLinks = document.getElementById("navbar-links");
+    if (navbarLinks.style.display === "flex") {
+        navbarLinks.style.display = "none";
+        navbarLinks.classList.remove("menu-open");
+    } else {
+        navbarLinks.style.display = "flex";
+        navbarLinks.classList.add("menu-open");
+    }
+}
+
+// Makes navbar behave properly on resize
+window.addEventListener('resize', function () {
+    var navbarLinks = document.getElementById("navbar-links");
+    if (window.innerWidth >= 768) {
+        navbarLinks.style.display = "flex";
+        navbarLinks.classList.remove("menu-open");
+    } else if (!navbarLinks.classList.contains("menu-open")) {
+        navbarLinks.style.display = "none";
+    }
+}
+);
